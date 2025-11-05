2d0c83996ea6f61e82c5e0b3638dcfe78f604e37
paul00415 <thepaul.work@gmail.com>
2025-11-05 22:25:01 +0900

    fix:bug

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { useMediaQuery } from './hooks/useMediaQuery';
 
 function App() {
   const { ref: sectionRef, inView: sectionInView } = useInView({ threshold: 0.1 });
-  const isBigScreen = useMediaQuery('(min-width: 1500px)');
+  const isBigScreen = useMediaQuery('(min-width: 1537px)');
   return (
     <div>
       <div className="h-50vh" style={{ backgroundImage: `url(/back3.jpg)` }}>
