cdab90d268d9a8d794cee7902fe59a63b9a3968c
paul00415 <thepaul.work@gmail.com>
2025-11-06 15:30:10 +0900

    style:contact

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -16,7 +16,7 @@ export const Contact = () => {
           />
           <PageTitle className="text-left mt-5 text-5xl" label="Work Together"></PageTitle>
         </div>
-        <div className="w-full px-4 md:px-32">
+        <div className="w-full px-4 md:px-10 lx:px-20">
           <SendMailCard />
         </div>
       </div>
